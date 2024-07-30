a0a5e26212ccef29bd85f26df4a1247494d8f6d4
vikas5567 <161954517+vikas5567@users.noreply.github.com>
2024-07-30 23:52:01 +0530

    first commit

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -15,9 +15,9 @@
           <li>
             <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
           </li>
-          <li>
+          <!-- <li>
             <a href="/dashboard" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page" >Dashboard</a>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
